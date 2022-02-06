14ab2741b1dda73bee910981a55cceceba7f080f
shihab shumon <shumonshihab62@gmail.com>
2022-02-06 12:22:53 +0600

    learned the basics of framer motion

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,19 +1,52 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { motion } from "framer-motion";
 
+const containerVariants = {
+  hidden: {
+    opacity: 0,
+  },
+  visible: {
+    opacity: 1,
+    transition: {
+      delay: 1.5,
+      duration: 1.5,
+      when: "beforeChildren",
+      staggerChildren: 1,
+    },
+  },
+};
+const buttonVariants = {
+  hover: {
+    scale: 1.1,
+    textShadow: " 0 0 8px rgb(255,255,255)",
+    boxShadow: "0 0 8px rgb(255,255,255)",
+    transition: {
+      duration: 0.3,
+      yoyo: Infinity,
+    },
+  },
+};
 const Home = () => {
   return (
-    <div className="home container">
-      <h2>
-        Welcome to Pizza Joint
-      </h2>
+    <motion.div
+      className="home container"
+      variants={containerVariants}
+      initial="hidden"
+      animate="visible"
+    >
+      <h2>Welcome to Pizza Joint</h2>
       <Link to="/base">
-        <button>
+        <motion.button
+          variants={buttonVariants}
+          whileHover="hover"
+          animate="visible"
+        >
           Create Your Pizza
-        </button>
+        </motion.button>
       </Link>
-    </div>
-  )
-}
+    </motion.div>
+  );
+};
 
 export default Home;
--- src/components/Toppings.js
+++ src/components/Toppings.js
@@ -1,6 +1,45 @@
+import { motion } from "framer-motion";
 import React from "react";
 import { Link } from "react-router-dom";
 
+const containerVariants = {
+  hidden: {
+    opacity: 0,
+    x: "100vw",
+  },
+  visible: {
+    opacity: 1,
+    x: 0,
+    transition: {
+      type: "spring",
+      delay: 0.5,
+    },
+  },
+};
+
+// const nextVariants = {
+//   hidden: {
+//     x: "-100vw",
+//   },
+//   visible: {
+//     x: 0,
+//     transition: {
+//       type: "spring",
+//       stiffness: 120,
+//     },
+//   },
+// };
+const buttonVariants = {
+  hover: {
+    scale: 1.1,
+    textShadow: " 0 0 8px rgb(255,255,255)",
+    boxShadow: "0 0 8px rgb(255,255,255)",
+    transition: {
+      duration: 0.3,
+      yoyo: Infinity,
+    },
+  },
+};
 const Toppings = ({ addTopping, pizza }) => {
   let toppings = [
     "mushrooms",
@@ -12,23 +51,40 @@ const Toppings = ({ addTopping, pizza }) => {
   ];
 
   return (
-    <div className="toppings container">
+    <motion.div
+      className="toppings container"
+      variants={containerVariants}
+      initial="hidden"
+      animate="visible"
+    >
       <h3>Step 2: Choose Toppings</h3>
       <ul>
         {toppings.map((topping) => {
           let spanClass = pizza.toppings.includes(topping) ? "active" : "";
           return (
-            <li key={topping} onClick={() => addTopping(topping)}>
+            <motion.li
+              key={topping}
+              onClick={() => addTopping(topping)}
+              whileHover={{
+                scale: 1.3,
+                color: "#f8e112",
+                originX: 0,
+                fontWeight: 800,
+              }}
+              transition={{ type: "spring", stiffness: 300 }}
+            >
               <span className={spanClass}>{topping}</span>
-            </li>
+            </motion.li>
           );
         })}
       </ul>
 
       <Link to="/order">
-        <button>Order</button>
+        <motion.button variants={buttonVariants} whileHover="hover">
+          Order
+        </motion.button>
       </Link>
-    </div>
+    </motion.div>
   );
 };
 
