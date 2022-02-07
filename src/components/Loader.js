541bdbe7a85e2074fad3b6d003686ac632bccd80
shihab shumon <shumonshihab62@gmail.com>
2022-02-07 17:35:07 +0600

    creating a loader

---
+++ src/components/Loader.js
@@ -0,0 +1,32 @@
+import { motion } from "framer-motion";
+import React from "react";
+
+const loaderVariants = {
+  animationOne: {
+    x: [-20, 20],
+    y: [0, -30],
+    transition: {
+      x: {
+        yoyo: Infinity,
+        duration: 0.5,
+      },
+      y: {
+        yoyo: Infinity,
+        duration: 0.25,
+      },
+    },
+  },
+};
+const Loader = () => {
+  return (
+    <>
+      <motion.div
+        className="loader"
+        variants={loaderVariants}
+        animate="animationOne"
+      ></motion.div>
+    </>
+  );
+};
+
+export default Loader;
